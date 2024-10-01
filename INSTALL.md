6783e65adeeaa056a89a5ef835df114f795778a8
nguyenvukhang <brew4k@gmail.com>
2024-10-01 13:17:18 +0800

    Scrub root

--- INSTALL.md
+++
@@ -1,3 +0,0 @@
-[To build from source](https://sw.kovidgoyal.net/alatty/build/)
-
-[Pre-built binaries](https://sw.kovidgoyal.net/alatty/binary/)
--- dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# dev.sh
-# Copyright (C) 2023 Kovid Goyal <kovid at kovidgoyal.net>
-#
-# Distributed under terms of the GPLv3 license.
-#
-
-exec go run bypy/devenv.go "$@"
